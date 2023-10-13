cc10a2f510317d88739b73156744afd5666a0f4b
RAMAKRISHNA <ramakrishnaprasad.2003@gmail.com>
2023-10-13 20:29:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ColorPicker from './Components_3/ColorPicker';
+import './Components_3/ColorPicker.css';
+function App() {
+  const colors = ["red", "blue", "green", "yellow", "purple"];
+  return (
+    <div className="App">
+      <h1>Color Picker App</h1>
+      <ColorPicker colors={colors} />
+    </div>
+
+  );
+}
+
+export default App;
